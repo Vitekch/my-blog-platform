49aed6bd41cc4e56a34b3e9164bbfca0a2ebd0d3
Viktor Chizhikov <vchizhikov@zuzex.com>
2022-01-24 13:46:23 +0300

    fix: MR

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <Header />
+    <Nav />
+    <v-main>
+      <router-view/>
+    </v-main>
+    <Footer />
+  </v-app>
 </template>
 
+<script lang="ts">
+import Vue from 'vue';
+import Header from './components/Layout/Header/index.vue';
+import Footer from './components/Layout/Footer/index.vue';
+import Nav from './components/Layout/Nav/index.vue';
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+    Header, Nav, Footer,
+  },
+
+  data: () => ({
+    nav: false,
+  }),
+});
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  :root{
   }
-}
+  :root.theme--dark{
+  }
 </style>
---
+++ src/components/Layout/Header/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app-bar
+      app
+      dense
+      elevation="2"
+    >
+    <v-app-bar-nav-icon @click="setNavVis"></v-app-bar-nav-icon>
+    <v-spacer></v-spacer>
+    <v-btn
+      small
+      color="var(--color-1)"
+      icon
+      class="ma-1"
+      @click="changeTheme"
+    >
+      <v-icon>
+        mdi-theme-light-dark
+      </v-icon>
+    </v-btn>
+    <v-btn
+      small
+      outlined
+      class="ml-12"
+    >
+      Login
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script src="./Header.ts"></script>
+<style src="./Header.scss" lang="scss"></style>
